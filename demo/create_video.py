d5156f95a0e535594af35a14c19a335ac2ec5068
Gaurav Tarlok Kakkar <gaurav21776@gmail.com>
2021-01-16 13:58:04 -0500

    checkpoint

---
+++ demo/create_video.py
@@ -0,0 +1,37 @@
+import cv2
+import os
+import numpy as np
+from src.response import Response
+from src.batch import Batch
+import pathlib
+from moviepy.editor import *
+
+def create_video_from_frames(batch, video_name):
+    
+    first_frame = np.array(batch.frames['data'][0])
+    height, width, layers = first_frame.shape
+
+    video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc(*'mp4v'), 1, (width, height))  
+  
+    # Appending the images to the video one by one 
+    for frame in batch.frames['data']:
+        image = cv2.UMat(np.array(frame, dtype=np.uint8))
+        video.write(image)  
+      
+    # Deallocating memories taken for window creation 
+    cv2.destroyAllWindows()  
+    video.release()  # releasing the video generated 
+
+    duration = len(batch.frames['data'])
+    print(duration)
+    edit_video(video_name, duration)
+
+    return video_name
+
+def edit_video(name, duration):
+    video = VideoFileClip(name).set_duration(duration)
+    filename = str(pathlib.Path().absolute() / name )
+    video.write_videofile(filename, fps = 30)
+    video.close()
+
+	
