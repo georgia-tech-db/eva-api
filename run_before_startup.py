689e603d9ef608d7dbda237b389ac8058d7644ee
Vivian Thiebaut <vivian.thiebaut@gmail.com>
2021-01-17 14:28:30 -0500

    script for loading video into EVA and creating UDF before stating up demo-api

---
+++ run_before_startup.py
@@ -0,0 +1,23 @@
+import asyncio
+import sys
+
+from typing import List
+from src.db_api import connect, connect_async
+
+async def run_async(query: List[str]):
+    hostname = '0.0.0.0'
+    port = 5432
+
+    connection = await connect_async(hostname, port)
+    cursor = connection.cursor()
+    for onequery in query:
+        await cursor.execute_async(onequery)
+        response = await cursor.fetch_one_async()
+        print('Query: %s' % onequery)
+        print(response)
+
+if __name__ == '__main__':
+    asyncio.run(run_async(['LOAD DATA INFILE "data/mnist/mnist.mp4" INTO mnist;',
+                           'CREATE UDF MnistCNN INPUT (Frame_Array NDARRAY (3, 28, 28)) OUTPUT (label TEXT(2)) TYPE Classificatio IMPL "udfs/digit_recognition.py";']))
+
+
