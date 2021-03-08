ba1ca658e10ee3e8deff9bd4f2e14ff3f6770809
lliu374 <lliu374@gatech.edu>
2021-03-08 01:02:02 -0500

    added init tests for eva api

---
+++ test/test_demo_query.py
@@ -0,0 +1,69 @@
+from demo.demo_api import RequestFrames
+from demo.demo_api import app
+import unittest
+from unittest.mock import patch
+import asyncio
+import re
+import os
+from demo.demo_api import create_query
+from src.batch import Batch
+import pandas as pd
+import numpy as np
+from src.response import Response
+import glob
+import json
+
+def create_dummy_batches(num_frames=10, start_id=0):
+    data = []
+    for i in range(num_frames):
+        data.append({'id': i + start_id,
+                     'data': np.array(
+                         np.ones((2, 2, 3)) * float(i + 1) * 25,
+                         dtype=np.uint8)})
+    return Batch(pd.DataFrame(data))
+
+async def mock_get_frames(query_list):
+    batch = create_dummy_batches(5)
+    return Response(200, batch)
+
+class TestDemoQuery(unittest.TestCase):
+    def setUp(self):
+        self.app = app
+        self.client = self.app.test_client()
+        #with self.app.app_context():   
+        self.app.config['TESTING'] = True
+        self.request_content = {"select": [{"text":"id"}, {"text":"data"}],
+             "from": "MyVideo",
+             "where": [{"text":"id==1"}]}
+
+    def test_api_send_name(self):
+        ret = self.client.get('/api/send_name')
+        pattern = r"{\"name\":\"result[0-9]+_[0-9]+\"}\n"
+        result = re.findall(pattern, str(ret.data.decode("utf-8")))
+        self.assertIsNotNone(result)
+
+    def test_api_send_video(self):
+        with open("dataset/result.mp4", "wb+") as f:
+            f.write(b"Test")
+        ret = self.client.get('/api/send_video/result')
+        self.assertEqual("Test", str(ret.data.decode("utf-8")))
+        os.remove("dataset/result.mp4")
+
+    def test_create_query(self):
+        query_statement = create_query(self.request_content)
+        self.assertEqual(query_statement, "SELECT id, data FROM MyVideo WHERE id==1;")
+
+    @patch('demo.demo_api.get_frames', mock_get_frames)
+    def test_api_queryeva(self):
+        ret = self.client.post('/api/queryeva', json=self.request_content)
+        #res = asyncio.run(request.get_frames(1))
+        pattern = r"{\"name\":\"result[0-9]+_[0-9]+\"}\n"
+        result = re.findall(pattern, str(ret.data.decode("utf-8")))
+        path_name = json.loads(ret.data)["name"]
+        path_name += ".mp4"
+        path_name = os.path.join("dataset", path_name)
+        self.assertTrue(os.path.exists(path_name))
+        os.remove(path_name)
+
+        
+
