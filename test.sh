0feed098866768c65097db44453b50de7d4dce2a
lliu374 <lliu374@gatech.edu>
2021-03-20 16:22:46 -0400

    update test.sh

--- test.sh
+++ test.sh
@@ -4,6 +4,8 @@
 # linter fails or the pytest fails. This is important for the Travis.
 
 # Run linter (checks code style)
+pip install flake8
+pip install coverage
 flake8 --select E,F src/ test/ --exclude src/filters,src/parser/evaql
 linter_code=$?
 # Run unit tests
