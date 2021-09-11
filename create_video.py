1764e43016a35a021a1307c7b31fcbaa227031dc
Suryatej Reddy <suryatej16102@iiitd.ac.in>
2021-09-10 23:09:47 -0400

    mid commit

--- create_video.py
+++ create_video.py
@@ -5,10 +5,44 @@ from src.response import Response
 from src.batch import Batch
 from moviepy.editor import *
 
-def create_video_from_frames(batch, name):
+import pickle
 
+PATH_TO_SAVE = "/home/pensieve/Documents/GaTech/Fall21/eva-ui/public/videos/"
+
+def create_video_from_frames(batch, name):
     video_name = name + '.mp4'
+    input_path = "ua_detrac.mp4"
+    vcap = cv2.VideoCapture(input_path)
+
+    width = int(vcap.get(3))
+    height = int(vcap.get(4))
+    fps = vcap.get(5)
+    print (width, height, fps)
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v') #codec
+    video=cv2.VideoWriter(video_name, fourcc, fps, (width,height))
     
+    frame_id = 0
+    # Capture frame-by-frame
+    ret, frame = vcap.read()  # ret = 1 if the video is captured; frame is the image
+    df = batch.frames
+    number_of_frames = 0
+    while ret:
+        temp = df[['pred_boxes', 'label']][df.id == frame_id]
+        if temp.size:
+            image = cv2.UMat(np.array(frame, dtype=np.uint8))
+            video.write(image)
+            number_of_frames += 1
+
+        frame_id+=1
+        ret, frame = vcap.read()
+
+    cv2.destroyAllWindows()  
+    video.release()  # releasing the video generated 
+    vcap.release()
+
+    '''
+    
+
     first_frame = np.array(batch.frames['data'][0])
     height, width, layers = first_frame.shape
 
@@ -23,18 +57,15 @@ def create_video_from_frames(batch, name):
     cv2.destroyAllWindows()  
     video.release()  # releasing the video generated 
 
-    duration = len(batch.frames['data'])
-    print(duration)
-    edit_video(video_name, duration)
+    '''
+    edit_video(video_name, fps)
 
     #delete video from root 
     os.remove(video_name)
 
-def edit_video(name, duration):
-    video = VideoFileClip(name).set_duration(duration)
-    if not os.path.exists('data'):
-        os.makedirs('data')
-    video.write_videofile("data/"+name, fps = 30)
+def edit_video(name,fps = 30):
+    video = VideoFileClip(name)
+    if not os.path.exists(PATH_TO_SAVE):
+        os.makedirs(PATH_TO_SAVE)
+    video.write_videofile(PATH_TO_SAVE+name, fps = fps)
     video.close()
-
-	
--- demo_api.py
+++ demo_api.py
@@ -6,53 +6,56 @@ from flask import Flask, request, make_response, send_file, jsonify
 from flask_restful import Resource, Api
 from src.db_api import connect_async
 from create_video import create_video_from_frames
+import pickle
 
 app = Flask(__name__)
 api = Api(app)
 
 input = []
 
+with open("modeldump.pickle", "rb") as f:
+    batch = pickle.load(f)
+
 class SendVideo(Resource):
-    def get(self, video_name):
-        print(video_name)
-        return send_file('data/'+video_name+'.mp4')
+	def get(self, video_name):
+		print(video_name)
+		return send_file('data/'+video_name+'.mp4')
 
 
 class RequestFrames(Resource):
 
-    request_id = 0
-    
-    def post(self):
-        RequestFrames.request_id = RequestFrames.request_id + 1
-        params = request.get_json()
-        query = create_query(params)
-        print(query)
-        #query = 'SELECT id,data FROM MyVideo WHERE id < 5;'
-        query_list = [query]
-        frames = asyncio.run(get_frames(query_list))
-        print("calling create video")
-        video_name = generate_video_name(params, RequestFrames.request_id)        
-        create_video_from_frames(frames._batch, video_name)
-        return jsonify({"name": video_name})
+	request_id = 0
+	
+	def post(self):
+		RequestFrames.request_id = RequestFrames.request_id + 1
+		params = request.get_json()
+		query = create_query(params)
+		# print(query)
+		# #query = 'SELECT id,data FROM MyVideo WHERE id < 5;'
+		# query_list = [query]
+		# frames = asyncio.run(get_frames(query_list))
+		# print("calling create video")
+		video_name = generate_video_name(params, RequestFrames.request_id)     
+		create_video_from_frames(batch, video_name)
+		return jsonify({"name": video_name  + ".mp4"})
 
 async def get_frames(query_list):
-    hostname = '0.0.0.0'
-    port = 5432
+	hostname = '0.0.0.0'
+	port = 5432
 	
-    connection = await connect_async(hostname, port)
-    cursor = connection.cursor()
-    for query in query_list:
-        print('Query: %s' % query)
-        await cursor.execute_async(query)
-        print("executed")
-        response = await cursor.fetch_one_async()
-        print("got response")
-    return response
-    
+	connection = await connect_async(hostname, port)
+	cursor = connection.cursor()
+	for query in query_list:
+		print('Query: %s' % query)
+		await cursor.execute_async(query)
+		print("executed")
+		response = await cursor.fetch_one_async()
+		print("got response")
+	return response
+	
 
 def create_query(req):
-	query = "SELECT "
-	
+	query = 'SELECT '
 	for s in req['select']:
 		query = query + s['text'] + ", "
 	query = query[:len(query) - 2] + " FROM " + req['from']
@@ -61,18 +64,18 @@ def create_query(req):
 		for s in req['where']:
 			query = query + s['text'] + " OR "
 		query = query[:len(query) - 4]
-	query = query + ";"
+	query = query + " ORDER BY id;"
 	
-	return query
+	return str(len(query)) + "|" + query
 
 def generate_video_name(query: str, num: int):
-    n = random.randrange(0,100000,1)
-    name = "result" + str(num) + "_" + str(n)
-    return name
+	n = random.randrange(0,100000,1)
+	name = "result" + str(num) + "_" + str(n)
+	return name
 
 	  
 api.add_resource(RequestFrames, '/api/queryeva')
 api.add_resource(SendVideo, '/api/send_video/<string:video_name>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
